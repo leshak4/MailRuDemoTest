89c722549ee886638c1d9c63692fb79142b064ea
Aleksei Abramenko <Aleksei_Abramenko@epam.com>
2016-11-07 18:27:47 +0300

    a little bit update

---
+++ src/main/java/email/elements/AbstractElement.java
@@ -0,0 +1,7 @@
+package email.elements;
+
+/**
+ * Created by Aleksei_Abramenko on 11/3/2016.
+ */
+public class AbstractElement {
+}
---
+++ src/test/java/email/blocks/AuthFormOnMainPageBlockTest.java
@@ -0,0 +1,8 @@
+import static org.testng.Assert.*;
+
+/**
+ * Created by Aleksei_Abramenko on 11/3/2016.
+ */
+public class AuthFormOnMainPageBlockTest {
+
+}
